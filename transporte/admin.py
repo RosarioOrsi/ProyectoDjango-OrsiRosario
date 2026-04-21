62a913b08f052ceb035845cfcbe0f0e3e02b42d0
Rosario Orsi <rosariobelenorsi@gmail.com>
2026-04-21 06:41:05 -0300

    Se registro modelos en admin

--- transporte/admin.py
+++ transporte/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Camion, Viaje, Chofer
 # Register your models here.
+
+#Registramos los modelos para que sean visibles en el panel de administracion
+admin.site.register(Camion)
+admin.site.register(Viaje)
+admin.site.register(Chofer)
