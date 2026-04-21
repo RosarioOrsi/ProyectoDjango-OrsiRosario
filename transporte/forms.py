593edee8be6093d6014bb6b5ca3896ab54602c66
Rosario Orsi <rosariobelenorsi@gmail.com>
2026-04-21 06:27:06 -0300

    Se añadio la descripcion a forms.py

--- transporte/forms.py
+++ transporte/forms.py
@@ -1,16 +1,19 @@
 from django import forms
 from .models import Camion, Viaje, Chofer
 
+#Formulario para dar de alta o modificar la informacion de una unidad
 class CamionForm(forms.ModelForm):
     class Meta:
         model = Camion
         fields = ['patente', 'modelo', 'tipo_unidad', 'chofer']
 
+#Formulario para dar de alta o modificar la informacion de un viaje
 class ViajeForm(forms.ModelForm):
     class Meta:
         model = Viaje
         fields = ['origen', 'destino', 'estado', 'camion']
 
+#Formulario para dar de alta o modificar la informacion de un chofer
 class ChoferForm(forms.ModelForm):
     class Meta:
         model = Chofer
